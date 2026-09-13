705cf5a3d8ef3017de46300b42e09bb449b390e6
gabrielcardoso-dev <sgabrielcardosoc@gmail.com>
2026-09-13 00:16:26 -0300

    feat: adiciona central de sugestões (react-app/src/main.tsx)

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
 import App from './App'
 import './index.css'
+import './suggestions.css'
 
 registerSW({ immediate: true })
 
